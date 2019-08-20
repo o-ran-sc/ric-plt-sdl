cf28227e814b7bcd17a65da5b649597bfe85f77e
Rolf Badorek <rolf.badorek@nokia.com>
2019-08-20 11:17:15 +0300

    Add first version
    First version of C++ SDL API.
    
    Refer README file for instructions how to generate API documentation.
    
    Current version will find database via environment variables, thus RIC
    DBaaS service must be up and running before starting the application
    pod which uses SDL C++ API.
    
    Change-Id: Ia3c95ac856b293827b680a9f6451a229f69f35f9
    Signed-off-by: Rolf Badorek <rolf.badorek@nokia.com>

---
+++ include/private/databaseconfigurationimpl.hpp
@@ -0,0 +1,49 @@
+/*
+   Copyright (c) 2018-2019 Nokia.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef SHAREDDATALAYER_DATABASECONFIGURATIONIMPL_HPP_
+#define SHAREDDATALAYER_DATABASECONFIGURATIONIMPL_HPP_
+
+#include "private/databaseconfiguration.hpp"
+
+namespace shareddatalayer
+{
+    class DatabaseConfigurationImpl: public DatabaseConfiguration
+    {
+    public:
+        DatabaseConfigurationImpl();
+
+        ~DatabaseConfigurationImpl() override;
+
+        void checkAndApplyDbType(const std::string& type) override;
+
+        void checkAndApplyServerAddress(const std::string& address) override;
+
+        DatabaseConfiguration::DbType getDbType() const override;
+
+        DatabaseConfigurationImpl::Addresses getServerAddresses() const override;
+
+        DatabaseConfiguration::Addresses getDefaultServerAddresses() const override;
+
+        bool isEmpty() const override;
+
+    private:
+        DbType dbType;
+        Addresses serverAddresses;
+    };
+}
+
+#endif
---
+++ include/private/tst/databaseconfigurationmock.hpp
@@ -0,0 +1,39 @@
+/*
+   Copyright (c) 2018-2019 Nokia.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef SHAREDDATALAYER_DATABASECONFIGURATIONMOCK_HPP_
+#define SHAREDDATALAYER_DATABASECONFIGURATIONMOCK_HPP_
+
+#include "private/databaseconfiguration.hpp"
+
+namespace shareddatalayer
+{
+    namespace tst
+    {
+        class DatabaseConfigurationMock: public DatabaseConfiguration
+        {
+        public:
+            MOCK_METHOD1(checkAndApplyDbType, void(const std::string& type));
+            MOCK_METHOD1(checkAndApplyServerAddress, void(const std::string& address));
+            MOCK_CONST_METHOD0(getDbType, DatabaseConfiguration::DbType());
+            MOCK_CONST_METHOD0(getServerAddresses, DatabaseConfiguration::Addresses());
+            MOCK_CONST_METHOD0(getDefaultServerAddresses, DatabaseConfiguration::Addresses());
+            MOCK_CONST_METHOD0(isEmpty, bool());
+        };
+    }
+}
+
+#endif
